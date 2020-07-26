53928bd94ac5189daef61562a9dfb6084b773a46
JozanLeClerc <bousset.rudy@gmail.com>
2020-07-26 16:09:15 +0200

    Removed saladjoe

--- inc/classic-contents.inc.php
+++ inc/classic-contents.inc.php
@@ -57,7 +57,6 @@ jo_sidebar() {
 		<ul>
 			<li><a href="<?php echo $prefix; ?>jozan/">jozan</a></li>
 			<li><a href="<?php echo $prefix; ?>salad/">salad</a></li>
-			<li><a href="<?php echo $prefix; ?>salad-joe/">salad-joe</a></li>
 			<hr>
 			<li><a href="https://jozanleclerc.xyz/" target="_blank">jozanleclerc.xyz</a></li>
 		</ul>
